52363b0766cdcc0ef069d9d467a01eba0bb59484
Gemma Acevedo <41479572+wzcwzc05@users.noreply.github.com>
2023-07-25 16:38:26 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,14 +1,6 @@
 #include <deque>
 #include <iostream>
 using namespace std;
-void debug(deque<int> q)
-{
-    deque<int>::iterator i;
-    for (i = q.begin(); i != q.end(); i++)
-        cout << *i << " ";
-    cout << endl;
-    return;
-}
 int main()
 {
     deque<int> a{1, 2, 3, 4, 5}, b{1, 2, 3}; // 声明两个vector变量a,b
@@ -18,15 +10,14 @@ int main()
     int n = a.size();                             // size函数返回容器大小
     cout << a.front() << " " << a.back() << endl; // front和back函数返回容器中第一个和最后一个元素
     if (a.empty() == false)
-    {                     // empty函数返回一个bool值，用以确定容器非空
-        a.push_back(101); // push_back函数在队列尾部插入一个元素
-        a.pop_back();          // pop_back函数在队列尾弹出一个元素
+    {                      // empty函数返回一个bool值，用以确定容器非空
+        a.push_back(101);  // push_back函数在队列尾部插入一个元素
+        a.pop_back();      // pop_back函数在队列尾弹出一个元素
         a.push_front(102); // push_front函数在队列头插入一个元素
         a.pop_front();     // pop_front函数在队列头弹出一个元素
-        b.clear();  // 清空b容器中的所有元素
-        swap(a, b); // 交换a,b容器中的内容
+        b.clear();         // 清空b容器中的所有元素
+        swap(a, b);        // 交换a,b容器中的内容
     }
-    debug(b);
     for (i = b.begin(); i != b.end(); i++)
         cout << *i << " ";
     cout << endl;
