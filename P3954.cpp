a488a1f661a2c49ebd0ab336247d6537922fc5b8
Gemma Acevedo <41479572+wzcwzc05@users.noreply.github.com>
2023-08-09 12:37:16 +0800

    Update

---
+++ P3954.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    cout << a * 0.2 + b * 0.3 + c * 0.5 << endl;
+    return 0;
+}
--- P5707.cpp
+++ P5707.cpp
@@ -1,10 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+void out(int hour, int minute)
+{
+    if (hour < 10)
+        cout << "0" << hour << ":";
+    else
+        cout << hour << ":";
+    if (minute < 10)
+        cout << "0" << minute << endl;
+    else
+        cout << minute << endl;
+    return;
+}
 int main()
 {
-    double a, b, c;
-    scanf("%lf %lf %lf", &a, &b, &c);
-    double p = (a + b + c) / 2;
-    printf("%.1lf", sqrt(p * (p - a) * (p - b) * (p - c)));
+    long long s, v, t;
+    cin>>s>>v;
+    if (s % v != 0)
+        t = s / v + 1;
+    else
+        t = s / v;
+    t += 10;
+    int hour = t / 60, left = 8;
+    int minute = t % 60;
+    while (true)
+    {
+        if (left - hour > 0)
+        {
+            out(left - hour - 1, 60 - minute);
+            break;
+        }
+        hour -= left;
+        left = 24;
+    }
     return 0;
 }
