3c79c50373b95979e25973f8c706913e919e1a6b
Gemma Acevedo <41479572+wzcwzc05@users.noreply.github.com>
2023-08-09 10:49:38 +0800

    顺序结构

---
+++ P5704.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    c-=32;
+    printf("%c",c);
+    return 0;
+}
---
+++ P5705.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double t,n;
+    scanf("%lf %lf",&t,&n);
+    printf("%.3lf\n%d",t/n,int(n*2));
+    return 0;
+}
