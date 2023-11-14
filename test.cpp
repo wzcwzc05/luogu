c9c95f002013d85dc7c28650b9ba47db4b1fa919
Gemma Acevedo <41479572+wzcwzc05@users.noreply.github.com>
2023-11-14 10:17:35 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -2,23 +2,15 @@
 using namespace std;
 using ll = long long;
 const ll N = 1e8 + 10;
-ll a, b, c, sum1, sum2, sum3;
+ll n, a[N];
+void func(int n){
+
+}
 int main()
 {
-    cin >> a >> b >> c;
-    for (ll i = 1; i <= a; i++)
-    {
-        sum1 = (i * i * i) % 998244353;
-        for (ll j = 1; j <= b; j++)
-        {
-            sum2 = sum1 * j * j % 998244353;
-            for (ll k = 1; k <= c; k++)
-            {
-                sum3 += sum2 * (c - k) % 998244353;
-                sum3 = sum3 % 998244353;
-            }
-        }
-    }
-    cout << sum3 << "\n";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    func(1);
     return 0;
 }
--- warmup/D.cpp
+++ warmup/D.cpp
@@ -7,7 +7,7 @@ int mp[501][501], temp[501][501];
 bool vis[501][501][3];
 int dx[5] = {1, 0, -1, 0};
 int dy[5] = {0, 1, 0, -1};
-struct node
+struct node // 结构体，用于存储节点，x,y为坐标,step为步数，isKey代表是否走过钥匙
 {
     int x;
     int y;
@@ -26,8 +26,10 @@ int main()
         {
             for (int j = 1; j <= m; j++)
             {
+                // 标记数组清零
                 vis[i][j][0] = 0;
                 vis[i][j][1] = 0;
+
                 char ch;
                 cin >> ch;
                 if (ch == '.')
@@ -37,33 +39,23 @@ int main()
                 if (ch == 'P')
                 {
                     startx = i, starty = j;
-                    mp[i][j] = 0;
+                    mp[i][j] = 0; // 记录起点位置
                 }
                 if (ch == 'K')
                 {
-                    mp[i][j] = 2;
+                    mp[i][j] = 2; // 数组中2代表该位置为key
                 }
                 if (ch == 'E')
                 {
                     endx = i, endy = j;
-                    mp[i][j] = 1;
+                    mp[i][j] = 1; // 记录终点位置
                 }
             }
         }
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= m; j++)
-                temp[i][j] = mp[i][j];
-        /*for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-                cout << mp[i][j] << " ";
-            cout << "\n";
-        }
-        break;*/
-        queue<node> q;
+        queue<node> q; // 队列
         node start;
         start.x = startx, start.y = starty, start.step = 0, start.isKey = 0;
-        q.push(start);
+        q.push(start);  // 起点加入队列
         step1 = -1;
         while (!q.empty())
         {
@@ -72,43 +64,34 @@ int main()
             int step = q.front().step;
             bool key = q.front().isKey;
 
-            if (x == endx && y == endy)
-            {
-                step1 = step;
-                break;
-            }
             q.pop();
             for (int i = 0; i < 4; i++)
             {
                 int xx = x + dx[i];
                 int yy = y + dy[i];
-                if (xx==endx && yy==endy && key==1)
+                if (xx == endx && yy == endy && key == 1)   // 如果已经到达终点且路径经过key，直接结束
                 {
-                    //cout << "yes\n";
-                    step1 = step+1;
+                    step1 = step + 1;
                     break;
                 }
-                if (xx >= 1 && xx <= n && yy >= 1 && yy <= m && mp[xx][yy] != 1)
+                if (xx >= 1 && xx <= n && yy >= 1 && yy <= m && mp[xx][yy] != 1)    //如果下一步节点在范围内且不为墙
                 {
                     node next;
-                    if (mp[xx][yy] == 2)
+                    if (mp[xx][yy] == 2)    // 如果该位置是key，那么改变node的key标记，如果不是，从上个节点继承
                         next.isKey = 1;
                     else
                         next.isKey = key;
-                    if (vis[xx][yy][next.isKey])
+                    if (vis[xx][yy][next.isKey])    // 被访问过直接跳过
                         continue;
-                    if (xx == endx && yy == endy && vis[xx][yy][1])
-                        continue;
-                    vis[xx][yy][next.isKey] = 1;
+                    vis[xx][yy][next.isKey] = 1;    // 标记已被访问
                     next.x = xx;
                     next.y = yy;
                     next.step = step + 1;
-                    q.push(next);
+                    q.push(next);   // 加入队列
                 }
             }
-            if (step1!=-1)
-            break;
-            // cout<<q.front().x<<" "<<q.front().y<<" "<<q.front().step<<"\n";
+            if (step1 != -1)    //
+                break;
         }
         if (step1 == -1)
         {
