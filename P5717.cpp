9f39d0738758b6171b879f1f5c37991e4d783a2b
UnderStarlight1 <2362405068@stu.suda.edu.cn>
2024-02-01 17:42:13 +0800

    Update

--- P5717.cpp
+++ P5717.cpp
@@ -5,23 +5,22 @@ int t[10];
 
 int main()
 {
-    cin>>t[0]>>t[1]>>t[2];
-    sort(t,t+3);
-    int a=t[0],b=t[1],c=t[2];
-    if (a+b<=c)
-    {
+    cin >> t[0] >> t[1] >> t[2];
+    sort(t, t + 3);
+    int a = t[0], b = t[1], c = t[2];
+    if (a + b <= c) {
         printf("Not triangle\n");
         return 0;
     }
-    if (a*a+b*b==c*c)
-    printf("Right triangle\n");
-    if (a*a+b*b>c*c)
-    printf("Acute triangle\n");
-    if (a*a+b*b<c*c)
-    printf("Obtuse triangle\n");
-    if (a==b || b==c || a==c)
-    printf("Isosceles triangle\n");
-    if (a==b && b==c)
-    printf("Equilateral triangle\n");
+    if (a * a + b * b == c * c)
+        printf("Right triangle\n");
+    if (a * a + b * b > c * c)
+        printf("Acute triangle\n");
+    if (a * a + b * b < c * c)
+        printf("Obtuse triangle\n");
+    if (a == b || b == c || a == c)
+        printf("Isosceles triangle\n");
+    if (a == b && b == c)
+        printf("Equilateral triangle\n");
     return 0;
 }
--- P5721.cpp
+++ P5721.cpp
@@ -3,11 +3,9 @@ using namespace std;
 int main()
 {
     int n, t = 1;
-    cin>>n;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n - i + 1; j++)
-        {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i + 1; j++) {
             if (t < 10)
                 cout << 0 << t;
             else
